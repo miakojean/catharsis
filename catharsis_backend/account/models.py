892e56925cd9bd3f9920e73be05e6e96ac39370e
miakojean <miakojeanyves@gmail.com>
2026-03-14 23:47:22 +0000

    work on the backend structure and front initialization

--- catharsis_backend/account/models.py
+++ catharsis_backend/account/models.py
@@ -1,8 +1,36 @@
 from django.db import models
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.utils import timezone
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("L'adresse email est obligatoire")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-class Customer(AbstractBaseUser):
-    email = models.EmailField()
-    phone_number = models.CharField(max_length=14) 
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+class Customer(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=14, blank=True, null=True)
+    
+    # Métriques et Statuts
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    # Identifiant de connexion
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [] # L'email est déjà requis via USERNAME_FIELD
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
